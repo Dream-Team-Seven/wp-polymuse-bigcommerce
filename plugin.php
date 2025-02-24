8bf1b335177232885f6567a3409f883bda0977f1
Pmacdon15 <pmacdonald15@gmail.com>
2025-02-24 15:57:07 -0700

    changed hook

--- plugin.php
+++ plugin.php
@@ -68,7 +68,7 @@ if (in_array('bigcommerce/bigcommerce.php', apply_filters('active_plugins', get_
 
         return $html;
     }
-    add_filter('bc_product_gallery_image_html', 'polymuse_add_model_and_thumbnail_to_gallery', 10, 4);
+    add_filter('bigcommerce/product/image/thumbnail', 'polymuse_add_model_and_thumbnail_to_gallery', 10, 4);
 
     function add_buttons_container()
     {
