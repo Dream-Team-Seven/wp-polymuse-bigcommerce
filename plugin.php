3b3c6feba86c79c3e88818985f8fecd7beff24fd
Pmacdon15 <pmacdonald15@gmail.com>
2025-02-24 16:40:40 -0700

    revert then changed order of functions

--- plugin.php
+++ plugin.php
@@ -17,22 +17,29 @@ if (!defined('ABSPATH')) {
 if (in_array('bigcommerce/bigcommerce.php', apply_filters('active_plugins', get_option('active_plugins')))) {
 
     error_log('BigCommerce plugin is active');
-    // Add 3D model to product gallery
 
+    // Add model-viewer script to header
+    function polymuse_add_model_viewer_script()
+    {
+        echo '<script type="module" src="https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js"></script>';
+    }
+    add_action('wp_head', 'polymuse_add_model_viewer_script');
+
+    // Add 3D model to product gallery
     function polymuse_modify_single_product_template($html, $product_id)
     {
         global $product;
         // Add 3D model viewer code here
         $model_url = "https://firebasestorage.googleapis.com/v0/b/polymuse-68692.appspot.com/o/models%2F20250205124059197%2FSheenChair.glb?alt=media&token=19402c2b-bb92-499e-83bf-d49c263bb09c";
         $model_thumbnail_url = plugins_url('3d-model-thumbnail.png', __FILE__);
-        $model_viewer_html = '<div class="bc-product-gallery__image polymuse-model-viewer">
-            <model-viewer src="' . esc_url($model_url) . '" alt="3D model of ' . esc_attr($product->get_name()) . '" auto-rotate camera-controls ar ar-modes="webxr scene-viewer quick-look" style="width: 100%; height: 100%;"></model-viewer>
-        </div>';
-        // Insert the 3D model viewer code after the product gallery images
-        $gallery_images_pattern = '/<div class="bc-product-gallery__images"(.*?)>/s';
-        $html = preg_replace_callback($gallery_images_pattern, function ($matches) use ($model_viewer_html) {
-            return $matches[0] . $model_viewer_html;
-        }, $html);
+        ?>
+        <div class="bc-product-gallery__image polymuse-model-viewer">
+            <model-viewer src="<?php echo esc_url($model_url); ?>"
+                alt="3D model of <?php echo esc_attr($product->get_name()); ?>" auto-rotate camera-controls ar
+                ar-modes="webxr scene-viewer quick-look" style="width: 100%; height: 100%;"></model-viewer>
+        </div>
+        <?php
+        $html .= ob_get_clean();
         return $html;
     }
     add_filter('bigcommerce/template/product/single', 'polymuse_modify_single_product_template', 10, 2);
@@ -51,12 +58,6 @@ if (in_array('bigcommerce/bigcommerce.php', apply_filters('active_plugins', get_
     }
     add_action('bc_before_add_to_cart_form', 'add_buttons_container');
 
-    // Add model-viewer script to header
-    function polymuse_add_model_viewer_script()
-    {
-        echo '<script type="module" src="https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js"></script>';
-    }
-    add_action('wp_head', 'polymuse_add_model_viewer_script');
 
     // Enqueue styles and scripts
     function polymuse_enqueue_assets()
