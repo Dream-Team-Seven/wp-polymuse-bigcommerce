0473a1b398a841c4a8d20a2745fc13a8cda3b9b1
Pmacdon15 <pmacdonald15@gmail.com>
2025-02-24 17:48:07 -0700

    fixed typo

--- plugin.php
+++ plugin.php
@@ -29,17 +29,19 @@ if (in_array('bigcommerce/bigcommerce.php', apply_filters('active_plugins', get_
     function polymuse_modify_single_product_template($html, $product_id)
     {
         global $product;
-        // Retrieve the default product content
-        $default_content = apply_filters('bigcommerce/template/product/single', '', $product_id);
-
-        // Add your custom div to the default content
-        $custom_div = '<div class="polymuse-custom-div"><h1>hey look at me</h1></div>';
-        $html = $default_content . $custom_div;
-
+        // Add 3D model viewer code here
+        $model_url = "https://firebasestorage.googleapis.com/v0/b/polymuse-68692.appspot.com/o/models%2F20250205124059197%2FSheenChair.glb?alt=media&token=19402c2b-bb92-499e-83bf-d49c263bb09c";
+        $model_thumbnail_url = plugins_url('3d-model-thumbnail.png', __FILE__);
+        ?>
+        <div class="polymuse-custom-div">
+            <h1>hey look at me</h1>
+        </div>
+        <?php
+        $html .= ob_get_clean();
+        error_log('polymuse_modify_single_product_template called html : ' . $html);
         return $html;
     }
     add_filter('bigcommerce/template/product/single', 'polymuse_modify_single_product_template', 10, 2);
-
 
     // Enqueue styles and scripts
     function polymuse_enqueue_assets()
